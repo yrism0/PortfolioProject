c73b48956855470ad2c8124c5f606071477b82e7
yrism0 <124402708+yrism0@users.noreply.github.com>
2025-11-06 01:55:11 +0000

    06-11 Update
    +Started implementing stain system
    +Started Enemy scripts
    =Splatters are pixelated

--- Assets/Scripts/Enemy/EnemyManager.cs
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -1,4 +1,5 @@
 using SplatterSystem;
+using System;
 using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
@@ -8,19 +9,54 @@ public class EnemyManager : MonoBehaviour
     [Header("Splatter")]
     public MeshSplatterManager splatter;
     public SplatterSettings SplatterSettings;
-    public Color deathSplatterColor = Color.red;
+    public Color splatterColour = Color.red;
+    public float splatOffset = 0f;
 
     [Header("Enemy Variables")]
-    private float eHealth = 100f;
+    [SerializeField] private float eHealth = 100f;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    public void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.tag == "pBullet")
+        {
+            Debug.Log("HIT");
+
+            if (eHealth > 0)
+            {
+                eHealth -= 25f;               
+            }
+            else if (eHealth <= 0)
+            {
+                Destroy(gameObject);
+                splatter.Spawn(SplatterSettings, transform.position, null, splatterColour);
+            }
+
+            // Take Damage
+
+            // IF health = 0 THEN enemyDeath()
+
+            // Splatter blood
+
+
+        }
+    }
+
+    private void EnemyDamage()
+    {
+
     }
 
     private void EnemyDeath()
     {
+        // Enemy dies
 
+        // Splatter more blood
+    }
+
+    public void SplatterHit(Vector2 direction)
+    {
+        Vector2 hitPos = (Vector2)transform.position + splatOffset * direction;
+        splatter.Spawn(SplatterSettings, hitPos, direction, splatterColour);
     }
    
 }
