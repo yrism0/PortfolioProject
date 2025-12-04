b41e2a4286ec1ccea1f641393e73075f85a1be0e
Sharkwaves <b01814233@studentmail.uws.ac.uk>
2025-12-04 17:37:42 +0000

    Pausemenu
    Pause menu added

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,4 +1,6 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 
@@ -12,7 +14,9 @@ public class UIManager : MonoBehaviour
     [SerializeField] private GameObject playerHUD;
     [SerializeField] private GameObject pauseMenu;
 
-    
+    public static bool isPaused;
+
+
 
 
     private void Awake()
@@ -29,17 +33,35 @@ public class UIManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused == false)
+            {
+                PauseGame();
+
+            }
+        }
     }
 
     public void PauseGame()
     {
+        
         pauseMenu.SetActive(true);
+        isPaused = true;
     }
 
     public void ResumeGame()
     {
         pauseMenu?.SetActive(false);
+        isPaused = false;
+    }
+
+    public void GoToMainMenu()
+    {
+        // Method used by Quit button on Pause Menu
+        Time.timeScale = 1f;
+        isPaused = false;
+        SceneManager.LoadScene("main menu");
     }
 
     public void HideHUD()
