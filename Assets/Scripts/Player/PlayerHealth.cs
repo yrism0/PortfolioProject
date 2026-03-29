e1c8449893bddbe1b39732da55bbfd3e5b1f1be1
yrism0 <124402708+yrism0@users.noreply.github.com>
2026-03-29 21:39:15 +0100

    Update 29-03
    ++FINISHED LEVEL 4
    ++NEW GAME END SCREEN DETAILS
    =FIXED SORTING LAYER ISSUES
    =FIXED TIMER ISSUES

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -58,7 +58,7 @@ public class PlayerHealth : MonoBehaviour
         
         isPlayerDead = false;
         meterPause = false;
-        maxHeatValue = 20f;
+        maxHeatValue = 15f;
         heatValue = maxHeatValue;        
         playerCollider = GetComponent<CircleCollider2D>();  
         impulseSource = GetComponent<CinemachineImpulseSource>();
